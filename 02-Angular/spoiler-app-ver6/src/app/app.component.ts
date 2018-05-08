1a67d169e2a5e037e141bede2220fb96f7a723c0
MaritzaRodriguez <maritza.rodriguez.enc@gmail.com>
2018-05-08 16:02:53 -0500

    Angular

--- 02-Angular/spoiler-app-ver6/src/app/app.component.ts
+++ 02-Angular/spoiler-app-ver6/src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import {el} from "@angular/platform-browser/testing/src/browser_util";
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+ nombre = 'Maritza';
+ apellido = 'Rodriguez';
+ edad = '28';
+ sueldo = '1';
+ fechaDeNacimiento = new Date('1990-02-05');
+  nombreBoton = 'Cambiar color';
+  nombreClase = 'sa-titulo';
+
+  cambiarClaseDeTitulo() {
+    const nombreClaseEsTitulo = this.nombreClase === 'sa-titulo';
+    if (nombreClaseEsTitulo) {
+      this.nombreClase = 'sa-subtitulo';
+    } else {
+      this.nombreClase = 'sa-titulo';
+    }
+  }
+
+  aplicarClase(valorInput: string) {
+    this.nombreClase = valorInput;
+ }
+
 }
--- 02-Angular/spoiler-app-ver6/src/app/app.module.ts
+++ 02-Angular/spoiler-app-ver6/src/app/app.module.ts
@@ -2,15 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+//@ALGO () //decorator
 @NgModule({
   declarations: [
     AppComponent
-  ],
+  ], //definir todos los componentes a usarse
   imports: [
     BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  ],// definimos todos los modulos
+  providers: [], // los servicios
+  bootstrap: [AppComponent] //componente Principal
 })
 export class AppModule { }
