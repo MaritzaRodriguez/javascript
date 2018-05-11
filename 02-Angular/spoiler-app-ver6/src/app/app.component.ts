ccfaf11022b5e81e16112dc8753642c388750460
MaritzaRodriguez <maritza.rodriguez.enc@gmail.com>
2018-05-11 18:20:34 -0500

    estilos  y banner

--- 02-Angular/spoiler-app-ver6/src/app/app.component.ts
+++ 02-Angular/spoiler-app-ver6/src/app/app.component.ts
@@ -7,25 +7,32 @@ import {el} from "@angular/platform-browser/testing/src/browser_util";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- nombre = 'Maritza';
- apellido = 'Rodriguez';
- edad = '28';
- sueldo = '1';
- fechaDeNacimiento = new Date('1990-02-05');
-  nombreBoton = 'Cambiar color';
-  nombreClase = 'sa-titulo';
 
-  cambiarClaseDeTitulo() {
-    const nombreClaseEsTitulo = this.nombreClase === 'sa-titulo';
-    if (nombreClaseEsTitulo) {
-      this.nombreClase = 'sa-subtitulo';
-    } else {
-      this.nombreClase = 'sa-titulo';
-    }
-  }
 
-  aplicarClase(valorInput: string) {
-    this.nombreClase = valorInput;
- }
+
+
 
 }
+  // nombre = 'Maritza';
+ // apellido = 'Rodriguez';
+ // edad = '28';
+ // sueldo = '1';
+ // fechaDeNacimiento = new Date('1990-02-05');
+ //  nombreBoton = 'Cambiar color';
+ //  nombreClase = 'sa-titulo';
+ //  clase = 'papa';
+ //
+ //  cambiarClaseDeTitulo() {
+ //    const nombreClaseEsTitulo = this.nombreClase === 'sa-titulo';
+ //    if (nombreClaseEsTitulo) {
+ //      this.nombreClase = 'sa-subtitulo';
+ //    } else {
+ //      this.nombreClase = 'sa-titulo';
+ //    }
+ //  }
+ //
+ //  aplicarClase(valorInput: string) {
+ //    this.nombreClase = valorInput;
+ // }
+
+
--- 02-Angular/spoiler-app-ver6/src/app/app.module.ts
+++ 02-Angular/spoiler-app-ver6/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import { PeliculaBannerComponent } from './peliculas/pelicula-banner/pelicula-banner.component';
 //@ALGO () //decorator
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PeliculaBannerComponent
   ], //definir todos los componentes a usarse
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],// definimos todos los modulos
   providers: [], // los servicios
   bootstrap: [AppComponent] //componente Principal
